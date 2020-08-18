e59bb284fc8f098a62b3eeb7428b62eb570dde11
Oliver <oliver.e.t@hotmail.com>
2020-08-18 16:03:13 +0200

    Add form validation

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace EmployeeManagement.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage="Name cannot exceed 50 characters")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage="Invalid Email format")]
+        [Display(Name = "Office Email")]
         public string Email { get; set; }
-        public Department Department { get; set; }
+        [Required]
+        public Department? Department { get; set; }
     }
 }
