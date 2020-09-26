e688b73c82d9d097e3bd26abf8b31f70872cefec
Oliver <oliver.e.t@hotmail.com>
2020-09-26 12:39:46 +0200

    Add edit employee functionality

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using EmployeeManagement.Models;
 using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
@@ -40,6 +41,50 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = this.employeeRepository.GetEmployee(id);
+            EmployeeEditViewModel viewModel = new EmployeeEditViewModel
+            {
+                Id = id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department,
+                ExistingPhotoPath = employee.PhotoPath,
+            };
+            return View(viewModel); 
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+                if (model.Photo != null)
+                {
+                    DeleteImage(model.ExistingPhotoPath); 
+                    employee.PhotoPath = ProceesUploadPhoto(model.Photo);
+                }
+                employeeRepository.Update(employee);
+                return RedirectToAction("details", new { id = employee.Id });
+            }
+            return View();
+        }
+
+        private void DeleteImage(string existingPhotoPath)
+        {
+            if (existingPhotoPath != null)
+            {
+                String filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", existingPhotoPath);
+                System.IO.File.Delete(filePath);
+            }
+        }
+
+        [HttpGet]
         public ViewResult Create()
         {
             return View();
@@ -50,25 +95,31 @@ namespace EmployeeManagement.Controllers
         {
             if (ModelState.IsValid)
             {
-                string uniqueFileName = null;
-                if (model.Photo != null)
-                {
-                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
-                    string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", uniqueFileName);
-                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
-                }
+                string uniqueFileName = ProceesUploadPhoto(model.Photo);
                 Employee newEmployee = new Employee
                 {
-                    Name=model.Name,
-                    Email=model.Email,
-                    Department=model.Department,
-                    PhotoPath=uniqueFileName
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    PhotoPath = uniqueFileName
                 };
                 employeeRepository.Add(newEmployee);
                 return RedirectToAction("details", new { id = newEmployee.Id });
             }
             return View();
         }
-            
+
+        private string ProceesUploadPhoto(IFormFile photo)
+        {
+            String uniqueFileName = null;
+            if (photo != null)
+            {
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", uniqueFileName);
+                photo.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+
+            return uniqueFileName;
+        }
     }
 }
---
+++ EmployeeManagement/ViewModels/EmployeeEditViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeeEditViewModel : EmployeeCreateViewModel
+    {
+        public int Id { get; set; }
+        public string ExistingPhotoPath { get; set; }
+    }
+}
